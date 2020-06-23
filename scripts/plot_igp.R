c4e7f071d1e27c1358720062c635e57d78d70ffe
mal2017 <matt.a.lawlor@gmail.com>
2020-06-22 22:02:01 -0400

    merge plotting and collecting for igp

--- scripts/plot_igp.R
+++ scripts/plot_igp.R
@@ -2,16 +2,24 @@ library(tidyverse)
 
 ifls <- snakemake@input
 
-ifls <- ifls %>% set_names(str_extract(ifls,regex("(?<=igp_).+(?=_grid)")))
+#ifls <- Sys.glob("test/igp_*comps_rep1_*qval_*.csv")
 
-df <- map_dfr(ifls, read_csv,.id="relation")
+fls <- tibble(file = ifls) %>%
+  mutate(comps = str_extract(file,"(?<=_).+(?=comps)")) %>%
+  mutate(rep = str_extract(file,"(?<=rep).+(?=_0)")) %>%
+  mutate(qval = str_extract(file,"(?<=rep\\d{1,2}_).+(?=qval)")) %>%
+  mutate(relation= str_extract(file,"(?<=qval_).+(?=\\.csv)"))
 
-g <- ggplot(df, aes(components, qval, fill=igp)) +
+df <- fls %>%
+  mutate(df = map(file,read_csv)) %>%
+  dplyr::select(-file,-comps,-qval,-rep) %>%
+  unnest(cols=c(df))
+
+g <- ggplot(df, aes(components, as.factor(qval), fill=igp)) +
 	geom_tile() +
 	facet_wrap(~relation) +
 	theme_minimal() +
 	scale_fill_viridis_c(option=3) +
 	theme(aspect.ratio=1)
-
 
 ggsave(snakemake@output[[1]],g)
