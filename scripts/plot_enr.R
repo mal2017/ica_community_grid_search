912ae28fc66ab43fbaea0484cb0f599049db7c74
mal2017 <matt.a.lawlor@gmail.com>
2020-07-01 11:59:27 -0400

    update plotting scripts to handle new filenames for multiple fdr cutoff approaches

--- scripts/plot_enr.R
+++ scripts/plot_enr.R
@@ -2,10 +2,10 @@ library(tidyverse)
 
 fls <- snakemake@input
 
-#ifls <- Sys.glob("test/enr_*comps_rep1_*qval_*.csv")
+#fls <- Sys.glob("test/enr_*comps_rep1_*qval_*.csv")
 
 fls <- tibble(file = fls) %>%
-  mutate(comps = str_extract(file,"(?<=_).+(?=comps)")) %>%
+  mutate(comps = str_extract(file,"(?<=fdr\\d{1}_).+(?=comps)")) %>%
   mutate(rep = str_extract(file,"(?<=rep).+(?=_0)")) %>%
   mutate(qval = str_extract(file,"(?<=rep\\d{1,2}_).+(?=qval)")) %>%
   mutate(ont= str_extract(file,"(?<=qval_).+(?=\\.csv)"))
--- scripts/plot_igp.R
+++ scripts/plot_igp.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 
-ifls <- snakemake@input
+fls <- snakemake@input
 
-#ifls <- Sys.glob("test/igp_*comps_rep1_*qval_*.csv")
+#fls <- Sys.glob("test/igp_*comps_rep1_*qval_*.csv")
 
-fls <- tibble(file = ifls) %>%
-  mutate(comps = str_extract(file,"(?<=_).+(?=comps)")) %>%
+fls <- tibble(file = fls) %>%
+  mutate(comps = str_extract(file,"(?<=fdr\\d{1}_).+(?=comps)")) %>%
   mutate(rep = str_extract(file,"(?<=rep).+(?=_0)")) %>%
   mutate(qval = str_extract(file,"(?<=rep\\d{1,2}_).+(?=qval)")) %>%
   mutate(relation= str_extract(file,"(?<=qval_).+(?=\\.csv)"))
@@ -15,7 +15,7 @@ df <- fls %>%
   dplyr::select(-file,-comps,-qval,-rep) %>%
   unnest(cols=c(df))
 
-g <- ggplot(df, aes(components, as.factor(qval), fill=igp)) +
+g <- ggplot(df, aes(components, qval, fill=igp)) +
 	geom_tile() +
 	facet_wrap(~relation) +
 	theme_minimal() +
