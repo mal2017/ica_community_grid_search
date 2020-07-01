a3a210a751f91af82ebe8f92ac6ec2032f8a4f63
mal2017 <matt.a.lawlor@gmail.com>
2020-07-01 11:58:45 -0400

    update qval calc script to handle both fdr cutoff approaches

--- scripts/qval_calc.R
+++ scripts/qval_calc.R
@@ -4,6 +4,7 @@ library(jsonlite)
 
 ifl <- snakemake@input[[1]]
 ofl <- snakemake@output[[1]]
+ica_ver <- snakemake@params[["ICAver"]]
 
 ica_raw <- read_csv(ifl) %>% dplyr::rename(X1=index)
 
@@ -13,11 +14,14 @@ qval_df <- ica_raw %>%
   mutate(qval = fdrtool(weight,plot=F,verbose = F)[['qval']]) %>%
   ungroup()
 
-qval_df <- qval_df %>%
-  group_by(module) %>%
-  mutate(side=ifelse(weight > mean(weight),"a","b")) %>%
-  ungroup() %>%
-  mutate(module=paste0(module,side)) %>%
-  dplyr::select(-side)
+if (ica_ver == 2) {
+  qval_df <- qval_df %>%
+    group_by(module) %>%
+    mutate(side=ifelse(weight > mean(weight),"a","b")) %>%
+    ungroup() %>%
+    mutate(module=paste0(module,side)) %>%
+    dplyr::select(-side)
+}
+
 
 write_csv(qval_df, ofl)
